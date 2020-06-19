89fff8b4162eaf0ca4eff2c15ff36c8a17c75ab4
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-19 11:51:19 -0400

    added route and form.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+import Home from "./Home";
+import Form from "./Form";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/pizza">pizza</Link>
+      </nav>
+      <div>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+      </div>
+      <Switch>
+        <Route path="/pizza">
+          <Form />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,4 @@
+import React from "react";
+const Form = (props) => {
+  return <form></form>;
+};
